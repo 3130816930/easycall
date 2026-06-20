bf5dc143abbd3eeb57589d57bd3395cc398c6eaf
gjb <3130816930@qq.com>
2026-06-20 20:59:19 +0800

    fix: add V2 flutterEmbedding, remove unused audioplayers dep

---
+++ fix_manifest.js
@@ -0,0 +1,9 @@
+﻿var f=require("fs");
+var m=f.readFileSync("D:/app/easycall/app/android/app/src/main/AndroidManifest.xml","utf8");
+m=m.replace("</application>",'<meta-data android:name="flutterEmbedding" android:value="2"/></application>');
+f.writeFileSync("D:/app/easycall/app/android/app/src/main/AndroidManifest.xml",m,"utf8");
+console.log("Added flutterEmbedding meta-data");
+
+// Also verify appearance at first glance
+var r=f.readFileSync("D:/app/easycall/app/android/app/src/main/res/values/styles.xml","utf8");
+console.log("styles.xml:", r.substring(0,200));
